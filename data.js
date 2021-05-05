034305fd53aa3a7894ff681a2a519ee786edc4d6
dinglydo <amrojjeh@gmail.com>
2021-05-05 09:38:37 -0500

    Updated site

--- data.js
+++ data.js
@@ -1,44 +1,20 @@
 var Projects = 
 {
-	"Software": 
-	[
-		{
-			"Name": "Book Club Bot",
-			"Description": "Manages book club things such as nominations and voting",
-			"ImagePath": "https://raw.githubusercontent.com/amrojjeh/BookClubBot/main/default_cover.jpg",
-			"Link": "https://github.com/amrojjeh/BookClubBot",
-			"Date": "January 2021"
-		},
-		{
-			"Name": "Discord Deception Games",
-			"Description": "A Town of Salem inspired discord bot. Includes a bunch of mini-games. Custom games coming soon, almost ready for release.",
-			"ImagePath": "bot.jpg",
-			"Link":"https://github.com/Persian-Eagle/Town-of-Salem-Bot",
-			"Date": "November 2020"
-		},
-		{
-			"Name": "Job Tracker",
-			"Description": "This is an application that is meant to track any person's job by keeping logs of when the shift starts and ends. The goal was to write it as modular as possible, while also including unit tests. However, due to a lack of time, this project has been cancelled.",
-			"ImagePath": "JobTracker.png",
-			"Link": "https://github.com/dinglydo/JobTracker",
-			"Date": "October 2019" // Latest update
-		},
-		{
-			"Name": "Chip8 Interpreter",
-			"Description": "This project is merely a highlight in my hobby. Last I worked on it was 2 years ago, so it's buggy and the code is ugly, however, it was a step up from anything else I was doing, hence why it's showcased here.",
-			"ImagePath": "Chip8.jpg",
-			"Link": "https://github.com/dinglydo/Chip8-Interpreter",
-			"Date": "May 2018"
-		}
-	],
 	"Computer Science":
 	[
+		{
+			"Name": "The Problem with AP CS",
+			"Description": "And other introductory classes",
+			"ImagePath": "https://miro.medium.com/max/1000/1*RfIRzExAjj1i2FOSPQ69ZA.png",
+			"Link": "https://medium.com/codex/the-problem-with-ap-cs-ad095f54960b",
+			"Date": "February 2021",
+		},
 		{
 			"Name": "Classes",
 			"Description": "And Why We Need Them",
 			"ImagePath": "https://miro.medium.com/max/2400/1*pfVYXitb6stSpP5cfdF5eA.png",
-			"Link": "https://medium.com/codex/classes-b9027a594ba9?sk=1a579f83a6b67a15f1b47ce0c4ba68f4",
-			"Date": "February 2021"
+			"Link": "https://medium.com/codex/classes-b9027a594ba9",
+			"Date": "May 2021"
 		},
 		{
 			"Name": "Recursion",
@@ -54,9 +30,6 @@ var Projects =
 			"Link": "https://amrojjeh.medium.com/1024-or-1000-22b7a01efe29?sk=dee2586d3bf0b2bccaa800e9e3035f7d",
 			"Date": "May 2020"
 		},
-	],
-	"Science":
-	[
 		{
 			"Name": "What Are Moles?",
 			"Description": "What are they!",
@@ -65,16 +38,23 @@ var Projects =
 			"Date": "November 2019"
 		},
 	],
-	"Humor":
+	"Stories":
 	[
+		{
+			"Name": "Shellfish",
+			"Description": "They say the truth is blunt. But what does that have to do with Shellfish?",
+			"ImagePath": "https://miro.medium.com/max/700/1*yh-b4shliNF2cNDo5L980A.png",
+			"Link": "https://amrojjeh.medium.com/shellfish-c1b0d40ffa02",
+			"Date": "April 2021",
+		},
 		{
 			"Name": "An Open Letter To Medium",
 			"Description": "A Very Personal Letter To Medium",
 			"ImagePath": "https://miro.medium.com/max/700/1*k93pu0wvTHVnBcGJy0NCqA.png",
 			"Date": "January 2021"
-		}
+		},
 	]
 }
 
-var SelectedCategory = "Software";
+var SelectedCategory = "Computer Science";
 var projectsElement = document.getElementById("projects");
