41a324f2c7aa69be51594b9db8451e0012d32852
dinglydo <amrojjeh@gmail.com>
2021-05-27 09:22:27 -0500

    redesign!

--- data.js
+++ data.js
@@ -1,61 +1,68 @@
 var Projects = 
-{
-	"Computer Science":
-	[
-		{
-			"Name": "The Problem with AP CS",
-			"Description": "And other introductory classes",
-			"ImagePath": "https://miro.medium.com/max/1000/1*RfIRzExAjj1i2FOSPQ69ZA.png",
-			"Link": "https://medium.com/codex/the-problem-with-ap-cs-ad095f54960b",
-			"Date": "February 2021",
-		},
-		{
-			"Name": "Classes",
-			"Description": "And Why We Need Them",
-			"ImagePath": "https://miro.medium.com/max/2400/1*pfVYXitb6stSpP5cfdF5eA.png",
-			"Link": "https://medium.com/codex/classes-b9027a594ba9",
-			"Date": "May 2021"
-		},
-		{
-			"Name": "Recursion",
-			"Description": "A simple explanation of recursion",
-			"ImagePath": "https://miro.medium.com/max/2400/0*Q46XI8JJjmOHMdL7",
-			"Link": "https://amrojjeh.medium.com/recursion-932aeac3f669?sk=b87ed7083f5daebbc91d8f1793ffe80b",
-			"Date": "January 2021"
-		},
-		{
-			"Name": "1024 or 1000?",
-			"Description": "There is a misunderstanding that occurs much too often between developers. Is a killobit 1024 or 1000 bits?",
-			"ImagePath": "https://miro.medium.com/max/2400/0*dvZ97m-KsG_MmvK-",
-			"Link": "https://amrojjeh.medium.com/1024-or-1000-22b7a01efe29?sk=dee2586d3bf0b2bccaa800e9e3035f7d",
-			"Date": "May 2020"
-		},
-		{
-			"Name": "What Are Moles?",
-			"Description": "What are they!",
-			"ImagePath": "https://miro.medium.com/max/700/1*2Y-yAERBFLUJYpy2Ndnpyg.png",
-			"Link": "https://amrojjeh.medium.com/what-are-moles-ec867a145ad5?sk=8ef37774eaca78928d05cb121752cdea",
-			"Date": "November 2019"
-		},
-	],
-	"Stories":
-	[
-		{
-			"Name": "Shellfish",
-			"Description": "They say the truth is blunt. But what does that have to do with Shellfish?",
-			"ImagePath": "https://miro.medium.com/max/700/1*yh-b4shliNF2cNDo5L980A.png",
-			"Link": "https://amrojjeh.medium.com/shellfish-c1b0d40ffa02",
-			"Date": "April 2021",
-		},
-		{
-			"Name": "An Open Letter To Medium",
-			"Description": "A Very Personal Letter To Medium",
-			"ImagePath": "https://miro.medium.com/max/700/1*k93pu0wvTHVnBcGJy0NCqA.png",
-			"Date": "January 2021"
-		},
-	]
-}
+[
+	{
+		"Name": "Understanding Long Division (For Teachers)",
+		"Description": "Most people who have yet to grow accustomed to a calculator, likely know how to divide any two numbers to obtain the decimal quotient. However, rarely it seems that the algorithm is actually explained, which means that the procedure is most often memorized without a sense of intuition. The hope of this article will be to introduce the intuition, and to finally demystify the mysteries of division.",
+		"ImagePath": "https://miro.medium.com/max/550/1*6Ey13T6t7T_SWI4DUXB66Q.png",
+		"Link": "https://medium.com/age-of-awareness/understanding-long-division-for-teachers-c00a982cc93c",
+		"Date": "May 2021",
+	},
+	{
+		"Name": "Everyone Learns Differently!!",
+		"Description": "If you’re a teacher, or even a student, you’ve likely heard this a million times. Everyone learns differently! You have visual learners, kinesthetic learners, aural learners, and some reader and writer type learners. A student can even be a mix of all the above! Taste all the flavors, so much diversity! Aren’t they all so yummy? Oh and that’s just VARK, but there are like 70 different schemes out there! I’m getting excited!",
+		"ImagePath": "https://miro.medium.com/max/5000/1*cBZIvNn3N8tXJl2E0CQTmg.png",
+		"Link": "https://medium.com/age-of-awareness/everyone-learns-differently-fb72975ca3f2",
+		"Date": "May 2021",
+	},
+	{
+		"Name": "The Problem with AP CS",
+		"Description": "And other introductory classes",
+		"ImagePath": "https://miro.medium.com/max/1000/1*RfIRzExAjj1i2FOSPQ69ZA.png",
+		"Link": "https://medium.com/codex/the-problem-with-ap-cs-ad095f54960b",
+		"Date": "February 2021",
+	},
+	{
+		"Name": "Classes",
+		"Description": "And Why We Need Them",
+		"ImagePath": "https://miro.medium.com/max/2400/1*pfVYXitb6stSpP5cfdF5eA.png",
+		"Link": "https://medium.com/codex/classes-b9027a594ba9",
+		"Date": "May 2021"
+	},
+	{
+		"Name": "Shellfish",
+		"Description": "They say the truth is blunt. But what does that have to do with Shellfish?",
+		"ImagePath": "https://miro.medium.com/max/700/1*yh-b4shliNF2cNDo5L980A.png",
+		"Link": "https://amrojjeh.medium.com/shellfish-c1b0d40ffa02",
+		"Date": "April 2021",
+	},
+	{
+		"Name": "An Open Letter To Medium",
+		"Description": "A Very Personal Letter To Medium",
+		"ImagePath": "https://miro.medium.com/max/700/1*k93pu0wvTHVnBcGJy0NCqA.png",
+		"Date": "January 2021"
+	},
+	{
+		"Name": "Recursion",
+		"Description": "A simple explanation of recursion",
+		"ImagePath": "https://miro.medium.com/max/2400/0*Q46XI8JJjmOHMdL7",
+		"Link": "https://amrojjeh.medium.com/recursion-932aeac3f669?sk=b87ed7083f5daebbc91d8f1793ffe80b",
+		"Date": "January 2021"
+	},
+	{
+		"Name": "1024 or 1000?",
+		"Description": "There is a misunderstanding that occurs much too often between developers. Is a killobit 1024 or 1000 bits?",
+		"ImagePath": "https://miro.medium.com/max/2400/0*dvZ97m-KsG_MmvK-",
+		"Link": "https://amrojjeh.medium.com/1024-or-1000-22b7a01efe29?sk=dee2586d3bf0b2bccaa800e9e3035f7d",
+		"Date": "May 2020"
+	},
+	{
+		"Name": "What Are Moles?",
+		"Description": "What are they!",
+		"ImagePath": "https://miro.medium.com/max/700/1*2Y-yAERBFLUJYpy2Ndnpyg.png",
+		"Link": "https://amrojjeh.medium.com/what-are-moles-ec867a145ad5?sk=8ef37774eaca78928d05cb121752cdea",
+		"Date": "November 2019"
+	},
+]
 
 // Removed software category
 var SelectedCategory = "Computer Science";
-var projectsElement = document.getElementById("projects");
--- index.js
+++ index.js
@@ -4,47 +4,14 @@ init();
 
 function init() // Initializes the site by loading everything neccessary
 {
-	renderCategories();
-	loadCategories();
-	select(Projects[SelectedCategory].Link);
-	loadProjects(SelectedCategory, projectsElement);
+	loadProjects(projects);
 }
 
-function renderCategories()
-{
-	for (var key in Projects)
-	{
-		var html = "<li id='" + key + "'onclick='categoryChangeEvent(this)' class>" + key + "</li>";
-		categories.innerHTML += html;
-	}
-}
-
-function loadCategories() // Loads the HTML document of the categories
-{
-	var categories = document.getElementById("categories");
-	for (var key in Projects)
-		Projects[key].Link = document.getElementById(key);
-}
-
-function select(element) // Underlines the category in CSS, hence why it's "selected"
-{
-	for (var key in Projects)
-		if (Projects[key].Link == element)
-		{
-			element.className = "selected";
-			SelectedCategory = key;
-		}
-		else Projects[key].Link.className = "";
-}
-
-function loadProjects(category, parent) // Loads projects from Projects and displays them according to the category
+function loadProjects(parent) // Loads projects from Projects and displays them according to the category
 {
 	parent.innerHTML = "";
-	for (var index in Projects[category])
-	{
-		if (index != "Link")
-		addProject(Projects[category][index], parent);
-	}
+	for (var project in Projects)
+		addProject(Projects[project], parent);
 }
 
 function addProject(project, parent) // Adds a project and displays
@@ -64,10 +31,4 @@ function generateHTML(name, description, imagePath, link, date) // Generates the
 	var date = "<i>Last updated: " + date + "</i>"
 	var p = "<p>" + description + "</p>";
 	return header + h1 + date + p + "</div>";
-}
-
-function categoryChangeEvent(element)
-{
-	select(element);
-	loadProjects(SelectedCategory, projectsElement);
 }
