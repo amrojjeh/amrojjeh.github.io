42256d9574da90c95e336c5771beef39f3aa9c33
dinglydo <amrojjeh@gmail.com>
2021-06-03 23:32:50 -0500

    metaa

--- build.py
+++ build.py
@@ -8,29 +8,9 @@ from markdown.treeprocessors import Treeprocessor
 from markdown.postprocessors import Postprocessor
 from markdown.extensions import Extension, fenced_code
 
-boilerplate_beginning = """<!DOCTYPE html>
-<html>
-<head>
-<meta charset='utf-8'>
-<meta http-equiv='X-UA-Compatible' content='IE=edge'>
-<title>LSC CS</title>
-<meta name='viewport' content='width=device-width, initial-scale=1'>
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="../../base.css">
-<link rel="stylesheet" media="screen" type="text/css" href="../monokai.css">
-<link rel="stylesheet" media="print" type="text/css" href="../friendly.css">
-<link rel='stylesheet' type='text/css' href='../article.css'>
-</head>
-<body>
-<header>
-<a class="underline" href="../../index.html">←</a>
-</header>
-"""
+base_url = "http://amrojjeh.github.io/"
 
-boilerplate_ending = """
-</body>
-</html>
-"""
+data_js = []
 
 class UnderlineProcessor(Treeprocessor):
 	def run(self, root):
@@ -41,26 +21,79 @@ class BoilerplateProcessor(Postprocessor):
 	def __init__(self, root, file):
 		self.root = root
 		self.file = file
+		with open(join(self.root, "meta.json"), "r") as f:
+			self.meta = json.loads(f.read())
+
+	def in_data_js(self):
+		for article in data_js:
+			if article["Link"] == self.root:
+				return True
+		return False		
+
+	def add_to_data_js(self):
+		if not self.in_data_js():
+			new = {
+			"Name": self.meta["cover"]["name"],
+			"Description": self.meta["cover"]["description"],
+			"Link": self.root,
+			"Date": self.meta["cover"]["date"],
+			}
+			data_js.append(new)
 
 	def run(self, text):
-		text = "<article>" + text + "</article>"
-		series_js = join(self.root, "series.js")
-		if exists(series_js):
-			text = self.generate_table_of_contents(series_js) + text
-		return boilerplate_beginning + text + boilerplate_ending
+		self.add_to_data_js()
+		return self.generate_html(text)
 
-	def generate_table_of_contents(self, series_js):
-		series = None
-		with open(series_js, "r") as f:
-			series = json.loads(f.read())
+	def generate_table_of_contents(self):
+		if self.meta.get("series") == None:
+			return ""
 		html = "<ul id='tableOfContents'>"
-		for v, k in series.items():
+		for v, k in self.meta["series"].items():
 			if k == self.file:
 				html += "<li><a class='selected'>" + v + "</a></li>"
 			else:
 				html += f"<li><a href='{k}' class='underline'>" + v + "</a></li>"				
 		html += "</ul>"
 		return html
+
+	def get_file_title(self):
+		if self.meta.get("series") == None:
+			return self.meta["cover"]["name"]
+		for title, file in self.meta["series"].items():
+			if self.file == file:
+				return title
+
+	def generate_html(self, text):
+		return f"""<!DOCTYPE html>
+<html>
+{self.generate_head()}
+<body>
+<header>
+<a class="underline" href="../../index.html">←</a>
+</header>
+{self.generate_table_of_contents()}
+<article>
+{text}
+</article>
+</body>
+</html>"""
+
+	def generate_head(self):
+		return f"""<head><meta charset='utf-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+<title>LSC CS</title>
+<meta name='viewport' content='width=device-width, initial-scale=1'>
+<meta content="{self.get_file_title()}" property="og:title">
+<meta content="{self.meta["cover"]["description"]}" property="og:description">
+<meta content="{base_url + f"{self.root}/{self.file}"}" property="og:url">
+<meta content="{base_url + f"{self.root}/cover.png"}" property="og:image">
+<meta content="#32CCFF" property="theme-color">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="../../base.css">
+<link rel="stylesheet" media="screen" type="text/css" href="../monokai.css">
+<link rel="stylesheet" media="print" type="text/css" href="../friendly.css">
+<link rel='stylesheet' type='text/css' href='../article.css'>
+</head>"""
 
 class MyExtensions(Extension):
 	def __init__(self, root, file):
@@ -71,16 +104,22 @@ class MyExtensions(Extension):
 		md.postprocessors.register(BoilerplateProcessor(self.root, self.file), "bp", 1)
 		md.treeprocessors.register(UnderlineProcessor(), "underline", 1)
 
-for root, _, files in os.walk("articles/"):
-	for file in files:
-		if file.endswith(".md"):
-			file_path = join(root, file)
-			with open(file_path, "r", encoding="utf-8") as md_file:
-				html_file_name = file.replace(".md", ".html")
-				with open(join(root, html_file_name), "w+", encoding="utf-8") as html_file:
-					extension_configs = {
-						"codehilite": {
-							"guess_lang": False,
-						},
-					}
-					html_file.write(markdown.markdown(md_file.read(), extensions=[MyExtensions(root, html_file_name), "toc", "codehilite", "tables", "md_in_html"], extension_configs=extension_configs, output_format="html5"))
+def build():	
+	for root, _, files in os.walk("articles/"):
+		for file in files:
+			if file.endswith(".md"):
+				file_path = join(root, file)
+				with open(file_path, "r", encoding="utf-8") as md_file:
+					html_file_name = file.replace(".md", ".html")
+					with open(join(root, html_file_name), "w+", encoding="utf-8") as html_file:
+						extension_configs = {
+							"codehilite": {
+								"guess_lang": False,
+							},
+						}
+						html_file.write(markdown.markdown(md_file.read(), extensions=[MyExtensions(root, html_file_name), "toc", "codehilite", "tables", "md_in_html"], extension_configs=extension_configs, output_format="html5"))
+	with open("data.js", "w+") as f:
+		f.write(f"let Articles = {json.dumps(data_js)}")
+if __name__ == "__main__":
+	build()
+	print("Site built")
--- index.js
+++ index.js
@@ -10,8 +10,17 @@ function init() // Initializes the site by loading everything neccessary
 function loadProjects(parent) // Loads projects from Projects and displays them according to the category
 {
 	parent.innerHTML = "";
+	var sorted_articles = []
 	for (var article in Articles)
-		addProject(Articles[article], parent);
+		sorted_articles.push(Articles[article])
+	sorted_articles.sort((a, b) => {
+		let a_val = (new Date(a["Date"])).valueOf();
+		let b_val = (new Date(b["Date"])).valueOf();
+		return b_val - a_val;
+	});
+	console.log(sorted_articles);
+	for (var article in sorted_articles)
+		addProject(sorted_articles[article], parent);
 }
 
 function addProject(project, parent) // Adds a project and displays
@@ -22,8 +31,8 @@ function addProject(project, parent) // Adds a project and displays
 
 function generateHTML(name, description, link, date) // Generates the HTML of a project
 {
-	var header = '<div><a href="articles/' + link + '/index.html"><img src="articles/' + link + '/cover.png' + '"></a>';
-	var h1 = '<h1><a class="underline" href="articles/' + link + '/index.html">' + name + '</a></h1>';
+	var header = '<div><a href="' + link + '/index.html"><img src="' + link + '/cover.png' + '"></a>';
+	var h1 = '<h1><a class="underline" href="' + link + '/index.html">' + name + '</a></h1>';
 	var date = "<i>Last updated: " + date + "</i>"
 	var p = "<p>" + description + "</p>";
 	return header + h1 + date + p + "</div>";
